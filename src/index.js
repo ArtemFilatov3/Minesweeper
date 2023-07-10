e649a0bccff5b1646b8a3076aebbb50b0453dd80
ArtemSyulatov <artem.syulatov.11111@gmail.com>
2023-07-10 20:25:33 +0500

    testing

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
       }
       gameBoard.push(rows);
+      console.log(gameBoard)
     }
   }
 
@@ -29,16 +30,18 @@ document.addEventListener("DOMContentLoaded", () => {
       row.forEach((cell, indexColumn) => {
         let paintCell = document.createElement("div");
         paintCell.id = "cell";
+        paintCell.addEventListener("click", () => {
+          handleClick(cell, indexRow, indexColumn);
+        });
         if (cell.isBombed && cell.isOpened) {
           paintCell.innerHTML = "x";
         }
         if (cell.number && !cell.isBombed && cell.isOpened) {
           paintCell.innerHTML = cell.number;
         }
-        paintCell.addEventListener("click", () => {
-          handleClick(cell, indexRow, indexColumn); 
-          console.log(paintCell)
-        });
+        if (cell.isOpened) {
+          paintCell.style.backgroundColor = "white";
+        }
         paintRow.append(paintCell);
       });
       board.append(paintRow);
@@ -118,115 +121,49 @@ document.addEventListener("DOMContentLoaded", () => {
   paintBoard();
 
   function handleClick(cell, indexRow, indexColumn) {
-    // console.log(cell);
-    cell.isOpened = true;
+    
+      
+    // gameBoard.forEach((Row, indexOfRows) => {
+    //   Row.forEach((elem, indexOfElems) => {
+    //     if (!elem.isBombed && !elem.isOpened && !elem.number) {
+    //       elem.isOpened = true;
+    //     }
+    //   });
+    // });  
+
+
+    if (cell.isBombed) {
+      gameBoard.forEach((indexRow) => {
+        indexRow.forEach((indexColumn) => {
+          if (indexColumn.isBombed) {
+            indexColumn.isOpened = true;
+          }
+        });
+      });
+      alert("Game Over");
+    } 
+    else if(!gameBoard[indexRow][indexColumn].isBombed && (gameBoard[indexRow][indexColumn].number || gameBoard[indexRow][indexColumn].number === 0 )){ 
+      cell.isOpened = true;
+      handleClick(cell, indexRow + 1 , indexColumn + 1) 
+    }else if(!gameBoard[indexRow][indexColumn].isBombed && (gameBoard[indexRow][indexColumn].number || gameBoard[indexRow][indexColumn].number === 0 )){
+      cell.isOpened = true;
+      handleClick(cell, indexRow , indexColumn + 1)
+    }else if(!gameBoard[indexRow][indexColumn].isBombed && (gameBoard[indexRow][indexColumn].number || gameBoard[indexRow][indexColumn].number === 0 )){
+      cell.isOpened = true;
+      handleClick(cell, indexRow - 1, indexColumn + 1)
+    }else if(!gameBoard[indexRow][indexColumn].isBombed && (gameBoard[indexRow][indexColumn].number || gameBoard[indexRow][indexColumn].number === 0 )){
+      cell.isOpened = true;
+      handleClick(cell, indexRow - 1, indexColumn - 1)
+    }else if(!gameBoard[indexRow][indexColumn].isBombed && (gameBoard[indexRow][indexColumn].number || gameBoard[indexRow][indexColumn].number === 0 )){
+      cell.isOpened = true;
+      handleClick(cell, indexRow - 1, indexColumn)
+    }else if(!gameBoard[indexRow][indexColumn].isBombed && (gameBoard[indexRow][indexColumn].number || gameBoard[indexRow][indexColumn].number === 0 )){
+      cell.isOpened = true;
+      handleClick(cell, indexRow + 1, indexColumn)
+    }
     paintBoard();
+    console.log(gameBoard[indexRow][indexColumn])
   }
 });
 
-// const gameBoard = [];
-// const BOMBS_COUNT = 10;
-// const WIDTH = 8;
-// const HEIGTH = 8;
-// let cell= Array.from(document.querySelectorAll("#cell")) ;
-// window.onload = function () {
-//   generateBoard(WIDTH, HEIGTH);
-//   genarateBombs();
-//   setNumbersInCell();
-//   paintBoard();
-//   cell
-//   console.log(cell);
-// };
-// console.log(cell);
-// function generateBoard(width, heigth) {
-//   for (let row = 0; row < heigth; row++) {
-//     const rows = [];
-//     for (let column = 0; column < width; column++) {
-//       rows.push({
-//         isBombed: false,
-//         number: 0,
-//         isOpened: false,
-//       });
-//     }
-//     gameBoard.push(rows);
-//   }
-// }
-
-// function paintBoard() {
-//   gameBoard.forEach((row) => {
-//     let paintRow = document.createElement("div");
-//     paintRow.id = "row";
-//     row.forEach((cell) => {
-//       let paintCell = document.createElement("div");
-//       paintCell.id = "cell";
-//       if (cell.isBombed && cell.isOpened) {
-//         paintCell.innerHTML = "x";
-
-//       }
-//       if (cell.number && !cell.isBombed && cell.isOpened) {
-//         paintCell.innerHTML = cell.number;
-//       }
-//       // cell.addEventListener('click',clickCell);
-//       paintRow.append(paintCell);
-//     });
-//     document.getElementById("board").append(paintRow);
-//   });
-// }
-
-// function genarateBombs() {
-//   let bombsRemaining = BOMBS_COUNT;
-//   while (bombsRemaining > 0) {
-//     const randomRowIndex = Math.floor(Math.random() * gameBoard.length);
-//     const randomColumnIndex = Math.floor(Math.random() * gameBoard[0].length);
-//     let cell = gameBoard[randomRowIndex][randomColumnIndex];
-//     if (!cell.isBombed) {
-//       cell.isBombed = true;
-//       bombsRemaining -= 1;
-//     }
-//   }
-// }
-
-// function setNumbersInCell() {
-//   gameBoard.forEach((row, indexRow) => {
-//     row.forEach((cell, indexColumn) => {
-//       if (cell.isBombed) {
-//         // 4-4
-//         if(gameBoard[indexRow-1] && gameBoard[indexRow-1][indexColumn-1]){
-//           gameBoard[indexRow-1][indexColumn-1].number +=1
-//         }
-//         // 4-5
-//         if(gameBoard[indexRow-1] && gameBoard[indexRow-1][indexColumn]){
-//           gameBoard[indexRow-1][indexColumn].number +=1
-//         }
-//         // 4-6
-//         if(gameBoard[indexRow-1] && gameBoard[indexRow-1][indexColumn+1]){
-//           gameBoard[indexRow-1][indexColumn+1].number +=1
-//         }
-//         // 5-4
-//         if(gameBoard[indexRow] && gameBoard[indexRow][indexColumn-1]){
-//           gameBoard[indexRow][indexColumn-1].number +=1
-//         }
-//         // 5-6
-//         if(gameBoard[indexRow] && gameBoard[indexRow][indexColumn+1]){
-//           gameBoard[indexRow][indexColumn+1].number +=1
-//         }
-//         // 6-4
-//         if(gameBoard[indexRow+1] && gameBoard[indexRow+1][indexColumn-1]){
-//           gameBoard[indexRow+1][indexColumn-1].number +=1
-//         }
-//         // 6-5
-//         if(gameBoard[indexRow+1] && gameBoard[indexRow+1][indexColumn]){
-//           gameBoard[indexRow+1][indexColumn].number +=1
-//         }
-//         // 6-6
-//         if(gameBoard[indexRow+1] && gameBoard[indexRow+1][indexColumn+1]){
-//           gameBoard[indexRow+1][indexColumn+1].number +=1
-//         }
-//       }
-//     });
-//   });
-// }
-
-// function clickCell(){
-
-// }
+//  использовать рекурсию, проверять каждую клетку вокруг, в параметрах поиграться с индексами колонок и столбцов
